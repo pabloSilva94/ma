ae4298cbdabd9d0858b41df19b6cbbb3b1e68eb3
Pablo Almeida <150823025+pabloSilva94@users.noreply.github.com>
2024-02-22 00:02:47 -0300

    sem sucesso no botão de login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,20 +1,25 @@
 import { Button, Input, notification, Space } from "antd";
 import Calendar from "../../assets/calendar.svg";
 import "./login.css";
+import { jwtDecode } from "jwt-decode";
 import {
   LockFilled,
   MailFilled,
   UserOutlined,
   IdcardFilled,
   PhoneFilled,
-  ArrowLeftOutlined
+  ArrowLeftOutlined,
 } from "@ant-design/icons";
 import { useEffect, useState } from "react";
 import { getIdLoja, getLojas, loginOwner } from "../../hooks/loja/useLoja";
 import { setUsetLocalStorage } from "../../utils/localStorageUtils";
 import { useNavigate } from "react-router-dom";
 import { createNewUser, loginUser } from "../../hooks/loja/useUsers";
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import {
+  GoogleLogin,
+  GoogleOAuthProvider,
+  useGoogleLogin,
+} from "@react-oauth/google";
 import axios from "axios";
 const nameApp = import.meta.env.VITE_APP_NOME_LOJA;
 function Login() {
@@ -22,13 +27,19 @@ function Login() {
   const [api, contextHolder] = notification.useNotification();
 
   const [user, setUser] = useState({ email: "", password: "" });
-  const [userRegister, setUserRegister] = useState({ name: "", cpf: "", phone: "", email: "", password: "" });
+  const [userRegister, setUserRegister] = useState({
+    name: "",
+    cpf: "",
+    phone: "",
+    email: "",
+    password: "",
+  });
   const [errorInput, setErrorinput] = useState("");
   const [errorInputRegiste, setErrorinputRegiste] = useState("");
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [isRegister, setIsRegister] = useState(false)
-  const [credential, setCredential] = useState(null)
+  const [isRegister, setIsRegister] = useState(false);
+  const [credential, setCredential] = useState([]);
   const handleLogin = async () => {
     setIsLoading(true);
     if (user.email === "" || user.password === "") {
@@ -82,14 +93,17 @@ function Login() {
 
       // Lidar com o erro de forma apropriada aqui
     }
-
-
-
   };
   const handleRegister = async () => {
     setIsLoading(true);
     const { name, cpf, phone, email, password } = userRegister;
-    if (email === "" || password === "" || name === "" || cpf === "" || phone === "") {
+    if (
+      email === "" ||
+      password === "" ||
+      name === "" ||
+      cpf === "" ||
+      phone === ""
+    ) {
       setIsLoading(false);
       setErrorinputRegiste("error");
       setTimeout(() => {
@@ -127,14 +141,13 @@ function Login() {
   //   console.log("setando dentro do useState", data); // Verifique se o estado data está sendo atualizado
   // };
   const getLojaApi = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const result = await getLojas();
     if (result.success === false) {
       return console.log(result.message);
     }
     console.log(result.lojaData);
-
-  }
+  };
   const openNotification = (placement) => {
     api.info({
       message: `Atenção`,
@@ -151,188 +164,194 @@ function Login() {
   };
 
   useEffect(() => {
-
     const getLojaId = async () => {
       const name = nameApp;
       const nameLoja = {
         name: name,
-      }
+      };
       try {
-        const result = await getIdLoja(nameLoja)
+        const result = await getIdLoja(nameLoja);
         if (result.success === false) {
-          return { success: false, message: result.message }
+          return { success: false, message: result.message };
         }
-        setData(result.lojaData)
+        setData(result.lojaData);
       } catch (error) {
-        return { success: false, message: error.message }
+        return { success: false, message: error.message };
       }
-    }
-    getLojaId()
-
-  }, [])
-  const getInfosUser = async (credentialResponse) => {
-    var credential = credentialResponse.credential
-    try {
-      // Verifique se o token de credencial está disponível
-      if (!credential) {
-        throw new Error('Token de credencial não disponível.');
-      }
-
-      // Faça uma solicitação para obter as informações do perfil do usuário
-      const response = await axios.get(`https://accounts.google.com/o/oauth2/v2/auth`, {
-        headers: {
-          Authorization: `Bearer ${credential}`,
-          Accept: 'application/json'
-        },
-
-      });
-
-      console.log(response.data);
-      // Atualize o estado com as informações do perfil do usuário
-      setCredential(response.data);
-    } catch (error) {
-      console.error('Erro ao obter informações do perfil do usuário:', error);
-    }
-  };
-  const getGoogleUrl = (credentialResponse) => {
-    const rootUrl = `https://accounts.google.com/o/oauth2/v2/auth`;
-    const cliId = credentialResponse.client_id
-    const options = {
-      redirect_uri: `http://localhost:5173/userdashboard`,
-      client_id: `${cliId}`,
-      access_type: "offline",
-      response_type: "code",
-      prompt: "consent",
-      scope: [
-        "https://www.googleapis.com/auth/userinfo.profile",
-        "https://www.googleapis.com/auth/userinfo.email",
-      ].join(" "),
-      state: from,
     };
+    getLojaId();
+  }, []);
+  const getInfosUser = async (credentialResponse) => {
+    var credentialApi = credentialResponse.credential;
+    var cliId = credentialResponse.clientId;
+    console.log(credentialApi);
+    try {
+      // Faça uma solicitação para obter as informações do perfil do usuário
+      const userInfo = await axios.get(
+        `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&include_granted_scopes=true&response_type=code&state=state_parameter_passthrough_value&redirect_uri=https%3A%2F%2F7181-2804-d51-4b13-2e00-9987-364b-c8d6-1658.ngrok-free.app%2Fuserdashboard&client_id=${cliId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${credentialApi}`,
+          },
+        }
+      );
 
-    const qs = new URLSearchParams(options);
-    console.log(qs);
-    return `${rootUrl}?${qs.toString()}`;
+      console.log(userInfo.data);
+      // Atualize o estado com as informações do perfil do usuário
+      // setCredential(response.data);
+    } catch (error) {
+      console.error("Erro ao obter informações do perfil do usuário:", error);
+    }
   };
+  // const getInfosUser = async (credentialResponse) => {
+  //   var credentialApi = credentialResponse.credential;
+  //   try {
+  //     // Faça uma solicitação para obter as informações do perfil do usuário
+  //     const decodedToken = jwtDecode(credentialApi);
+  //     console.log(decodedToken);
+  //     setCredential(decodedToken);
+  //     return { success: true, message: "deu certo" };
+  //     // Atualize o estado com as informações do perfil do usuário
+  //     // setCredential(response.data);
+  //   } catch (error) {
+  //     console.error("Erro ao obter informações do perfil do usuário:", error);
+  //   }
+  // };
 
   return (
     <div className="lContainer">
       <div className="formLogin">
-        {!isRegister && <>
-          <form>
-            <h1>Seja bem Vindo </h1>
-            <Input
-              placeholder="E-mail"
-              className="inptForm"
-              prefix={<MailFilled />}
-              status={errorInput}
-              value={user.email}
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-            />
-            <Input.Password
-              placeholder="Senha"
-              className="inptForm"
-              prefix={<LockFilled />}
-              status={errorInput}
-              value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              type="password"
-            />
-            <Button
-              className="btnLogin"
-              type="primary"
-              loading={isLoading}
-              onClick={handleLogin}
-            >
-              Entrar
-            </Button>
-            {contextHolder}
-
-          </form>
-          <Space direction="vertical" align="center">
-            <Button type="link" onClick={() => setIsRegister(true)}>Cadastrar</Button>
-            <GoogleOAuthProvider clientId="141263858504-o467hgk0hveld3i5399ev3srrufjdi9b.apps.googleusercontent.com">
-              <GoogleLogin
-                onSuccess={credentialResponse => {
-                  getGoogleUrl(credentialResponse)
-                  // console.log(credential);
-                  console.log(credentialResponse);
-                }}
-                onError={() => {
-                  console.log('Login Failed');
-                }}
-              />;
-            </GoogleOAuthProvider>;
-          </Space>
-        </>}
-        {isRegister && <>
-          <form>
-            {/* {console.log(data[0].id)} */}
-            <Space style={{ width: "30%", justifyContent: "space-between" }}>
-              <Button type="link" onClick={() => setIsRegister(false)}><ArrowLeftOutlined /></Button>
-              <h1>Cadastro</h1>
-            </Space>
-            <Input
-              placeholder="Nome"
-              className="inptForm"
-              prefix={<UserOutlined />}
-              status={errorInputRegiste}
-              value={userRegister.name}
-              onChange={(e) => setUserRegister({ ...userRegister, name: e.target.value })}
-            />
-            <Space style={{ width: "30%" }}>
+        {!isRegister && (
+          <>
+            <form>
+              <h1>Seja bem Vindo </h1>
               <Input
-                placeholder="Cpf"
+                placeholder="E-mail"
                 className="inptForm"
-                prefix={<IdcardFilled />}
-                status={errorInputRegiste}
-                value={userRegister.cpf}
-                onChange={(e) => setUserRegister({ ...userRegister, cpf: e.target.value })}
+                prefix={<MailFilled />}
+                status={errorInput}
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
               />
-              <Input
-                placeholder="Telefone"
+              <Input.Password
+                placeholder="Senha"
                 className="inptForm"
-                prefix={<PhoneFilled />}
-                status={errorInputRegiste}
-                value={userRegister.phone}
-                onChange={(e) => setUserRegister({ ...userRegister, phone: e.target.value })}
+                prefix={<LockFilled />}
+                status={errorInput}
+                value={user.password}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                type="password"
               />
-            </Space>
-            <Input
-              placeholder="E-mail"
-              className="inptForm"
-              prefix={<MailFilled />}
-              status={errorInputRegiste}
-              value={userRegister.email}
-              onChange={(e) => setUserRegister({ ...userRegister, email: e.target.value })}
-            />
-            <Input.Password
-              placeholder="Senha"
-              className="inptForm"
-              prefix={<LockFilled />}
-              status={errorInputRegiste}
-              value={userRegister.password}
-              onChange={(e) => setUserRegister({ ...userRegister, password: e.target.value })}
-              type="password"
-            />
-            <Button
-              className="btnLogin"
-              type="primary"
-              loading={isLoading}
-              onClick={handleRegister}
-            >
-              Entrar
-            </Button>
-            {contextHolder}
+              <Button
+                className="btnLogin"
+                type="primary"
+                loading={isLoading}
+                onClick={handleLogin}
+              >
+                Entrar
+              </Button>
+              {contextHolder}
+            </form>
+            <Space direction="vertical" align="center">
+              <Button type="link" onClick={() => setIsRegister(true)}>
+                Cadastrar
+              </Button>
+              <GoogleOAuthProvider clientId="141263858504-o467hgk0hveld3i5399ev3srrufjdi9b.apps.googleusercontent.com">
+                <GoogleLogin
+                  onSuccess={(credentialResponse) => {
+                    getInfosUser(credentialResponse);
+                  }}
+                  onError={() => {
+                    console.log("Login Failed");
+                  }}
+                />
+              </GoogleOAuthProvider>
 
-          </form>
-        </>}
+              <h1>{credential.name ? credential.name : ""}</h1>
+            </Space>
+          </>
+        )}
+        {isRegister && (
+          <>
+            <form>
+              {/* {console.log(data[0].id)} */}
+              <Space style={{ width: "30%", justifyContent: "space-between" }}>
+                <Button type="link" onClick={() => setIsRegister(false)}>
+                  <ArrowLeftOutlined />
+                </Button>
+                <h1>Cadastro</h1>
+              </Space>
+              <Input
+                placeholder="Nome"
+                className="inptForm"
+                prefix={<UserOutlined />}
+                status={errorInputRegiste}
+                value={userRegister.name}
+                onChange={(e) =>
+                  setUserRegister({ ...userRegister, name: e.target.value })
+                }
+              />
+              <Space style={{ width: "30%" }}>
+                <Input
+                  placeholder="Cpf"
+                  className="inptForm"
+                  prefix={<IdcardFilled />}
+                  status={errorInputRegiste}
+                  value={userRegister.cpf}
+                  onChange={(e) =>
+                    setUserRegister({ ...userRegister, cpf: e.target.value })
+                  }
+                />
+                <Input
+                  placeholder="Telefone"
+                  className="inptForm"
+                  prefix={<PhoneFilled />}
+                  status={errorInputRegiste}
+                  value={userRegister.phone}
+                  onChange={(e) =>
+                    setUserRegister({ ...userRegister, phone: e.target.value })
+                  }
+                />
+              </Space>
+              <Input
+                placeholder="E-mail"
+                className="inptForm"
+                prefix={<MailFilled />}
+                status={errorInputRegiste}
+                value={userRegister.email}
+                onChange={(e) =>
+                  setUserRegister({ ...userRegister, email: e.target.value })
+                }
+              />
+              <Input.Password
+                placeholder="Senha"
+                className="inptForm"
+                prefix={<LockFilled />}
+                status={errorInputRegiste}
+                value={userRegister.password}
+                onChange={(e) =>
+                  setUserRegister({ ...userRegister, password: e.target.value })
+                }
+                type="password"
+              />
+              <Button
+                className="btnLogin"
+                type="primary"
+                loading={isLoading}
+                onClick={handleRegister}
+              >
+                Entrar
+              </Button>
+              {contextHolder}
+            </form>
+          </>
+        )}
       </div>
       <div className="imgBg">
         <h1>{nameApp}</h1>
         <img src={Calendar} />
       </div>
-    </div >
+    </div>
   );
 }
 
